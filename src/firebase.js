a195c12a992072243b00a1b708fc39714cceb2d5
armaanx <103936457+armaanx@users.noreply.github.com>
2023-02-09 08:24:11 +0530

    stored the api keys in the .env file

--- src/firebase.js
+++ src/firebase.js
@@ -9,12 +9,12 @@ import {
 import { getFirestore, doc, getDoc, setDoc } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCo3543FgtYL1FTSIuRaDV5xu6sPd5Dl0U",
-  authDomain: "passprotect-5c085.firebaseapp.com",
-  projectId: "passprotect-5c085",
-  storageBucket: "passprotect-5c085.appspot.com",
-  messagingSenderId: "593607298592",
-  appId: "1:593607298592:web:dc1fd68dd6af1f4aa93645",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
